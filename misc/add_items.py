e7e91d4e0268c6e6579987214c782c6d85c2b23a
Igor Znamensky <igor.znamensky@gmail.com>
2020-01-29 13:53:53 +0300

    Гл.3 - Создание собственных фильтров

--- misc/add_items.py
+++ misc/add_items.py
@@ -53,7 +53,6 @@ def add_posts(Num: int, users):
         author = users[random_index_users]
         body = ''.join(fake.texts(nb_texts=3, max_nb_chars=200, ext_word_list=None))
         status = 'published'
-        tag = tags[random_index_tags]
 
         Post.objects.create(title=title,
                             slug=slug,
